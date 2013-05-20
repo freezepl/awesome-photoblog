f7d3240c8e38000b8f8d0e419735aa5d51314bda
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2013-05-20 22:26:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomePhotoblog::Application.config.secret_token = '866d34a8ae9e23e4ffbba14d6eed70e33db0031fca343ce899c17ac5d9c4cc13cfdd04527f1e28d6b4b8c76287f3a2b68828de56ab92812e2a25cb9408ec83d5'
