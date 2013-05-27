0a3a6c87756c1eba5f29c6ae1466c8efdeb56e5e
Łukasz Młodzik <lukasz.mlodzik@gmail.com>
2013-05-27 20:35:26 +0200

    Installed and configured devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module AwesomePhotoblog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # *#*#************ heroku stuff
+    config.assets.initialize_on_precompile = false
+
   end
 end
